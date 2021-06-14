a5f265b88c022adf0d76d5c93440d3bbc9f22b72
Rohit Sharma <rohit@numberzs-MacBook-Pro.local>
2021-06-14 10:16:39 +0530

    secret key updated

--- services/jwt/index.js
+++ services/jwt/index.js
@@ -1,16 +1,16 @@
 const jwt = require('jsonwebtoken');
 
-const generateAccessToken = (userObj) => jwt.sign(userObj, process.env.JWT_SECRET_KEY, {
+const generateAccessToken = (userObj) => jwt.sign(userObj, process.env.JWT_SECRET_KEY || 'ZHjYm0Bym66lACQFemHqWimXAkWnitUm', {
   algorithm: 'HS256',
   expiresIn: '1d',
 });
 
-const destroyAccessToken = (userObj) => jwt.sign(userObj, process.env.JWT_SECRET_KEY, {
+const destroyAccessToken = (userObj) => jwt.sign(userObj, process.env.JWT_SECRET_KEY || 'ZHjYm0Bym66lACQFemHqWimXAkWnitUm', {
   algorithm: 'HS256',
   expiresIn: 5,
 });
 
-const authenticateAccessToken = (token) => jwt.verify(token, process.env.JWT_SECRET_KEY);
+const authenticateAccessToken = (token) => jwt.verify(token, process.env.JWT_SECRET_KEY || 'ZHjYm0Bym66lACQFemHqWimXAkWnitUm');
 
 module.exports = {
   generateAccessToken,
