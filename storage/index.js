6f68ac7e65b9b13016584c9467e2484cd9bffd41
Rohit Sharma <rohit@numberzs-MacBook-Pro.local>
2021-06-14 10:07:57 +0530

    connection string hardcoded

--- storage/index.js
+++ storage/index.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
 
 const connectionPool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  // process.env.DATABASE_URL,
+  connectionString: 'postgres://qvgovdhnjxljio:b0d198d422861758b395d2d20676cfad268f5a1ad5bb04fcbe4d4de4fe799f21@ec2-52-86-25-51.compute-1.amazonaws.com:5432/dd8s1bj6vkllpr?ssl=true',
 });
 
 module.exports = connectionPool;
