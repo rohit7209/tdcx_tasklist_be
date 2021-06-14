a0239249909e9c2cce4e1b183a577e5c98e38d13
Rohit Sharma <rohit@numberzs-MacBook-Pro.local>
2021-06-14 19:59:47 +0530

    db url changed

--- storage/index.js
+++ storage/index.js
@@ -1,8 +1,7 @@
 const { Pool } = require('pg');
 
 const connectionPool = new Pool({
-  // process.env.DATABASE_URL,
-  connectionString: 'postgres://qvgovdhnjxljio:b0d198d422861758b395d2d20676cfad268f5a1ad5bb04fcbe4d4de4fe799f21@ec2-52-86-25-51.compute-1.amazonaws.com:5432/dd8s1bj6vkllpr',
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
